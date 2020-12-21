59cdb14877fdee15158ef4962661cc73e8dfb018
arthurhtml <59116734+arthurhtml@users.noreply.github.com>
2020-12-21 14:43:04 +0100

    final code

---
+++ PROJET_TUT/PHP/Economie.php
@@ -0,0 +1,264 @@
+<?php
+$title = 'Économie';
+include ('header.php');
+?>
+
+
+<div class="container">
+    <div class="titrePrinci">
+        <div class="row text-center align-items-center">
+            <div class="col-md-12" id="enjeuxeco">
+                <h1>
+                    Les enjeux économiques
+                </h1>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container">
+    <div class="row text-center align-items-center">
+        <div class="col-md-12">
+            <div class="titrePrinci">
+                <h3>
+                    L'histoire économique
+                </h3>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container">
+    <div class="row text-center align-items-center">
+        <div class="col-md-6">
+            <img src="../IMG/AppleDet.png" alt="image Apple" class="img-fluid rounded" id="samsungEcoImg">
+        </div>
+        <div class="col-md-6">
+            <img src="../IMG/Samsung.png" alt="image Samsung" class="img-fluid rounded">
+        </div>
+    </div>
+</div>
+
+<div class="container">
+    <div class="row text-justify">
+        <div class="col-md-6">
+            <p class="reveal">
+                Apple, cette société Américaine que le monde entier connait a été créée le 1er
+                Avril 1976 dans le garage de la maison d'époque de Steve Jobs. Pour avoir plus d'informations
+                sur l'histoire générale de cette société il y a la page <a href="apple.php">Apple</a> qui
+                est dédiée a cela.
+                Apple étant une multinationale, une société importante et imposante de ces 10
+                dernières années. Elle a énormément évoluée et contribué aux technologies d'aujourd'hui.
+                Apple a acheté une multitude de petites entreprises
+                pour pouvoir se répartir le travail au maximum et ainsi maximiser les bénéfices aux niveaux
+                monétaire et du temps de création. C'est aussi pour cette raison qu'elle ne compte que 147 000 employés.
+                C'est inférieur a la moitié des employés du Groupe Samsung.
+                Un des plus gros et des plus populaires achats d'Apple a été la société Beats Electronics
+                pour un montant de 3 milliards de dollars afin de s'approprier une entreprise qui évoluait
+                populairement. Dans le but d'arriver sérieusement sur le marché de l'audio qui évoluait énormément.
+                Tout ceci bien sur dans un but de pouvoir donner la possibilité à l'utilisateur
+                d'améliorer au maximum la qualité auditive.
+                C'est avec cette stratégie qu'Apple a pu majoritairement devenir la société qu'elle est aujourd'hui.
+            </p>
+        </div>
+        <div class="col-md-6" id="textSamsung">
+            <p class="reveal">
+                Samsung, présentée également sur la page <a href="samsung.php">Samsung</a>
+                a été créée le 1er Mars 1938 en Corée du Sud.
+                Au départ l'entreprise va investir dans d'autres domaines comme le commerce, les assurances ou encore la sécurité.
+                Ce n'est que plus tard que sera créé le Groupe Samsung comprenant toutes ses filiales.
+                A l'inverse d'Apple, le Groupe Samsung n'a pas acheté de sociétés,
+                il les a créés, par exemple ici Samsung Electronics, Samsung Engineering,
+                Samsung Heavy Industries, Samsung Life et bien d'autres encore pour les
+                différents domaines du Groupe Samsung et de ses 400 000 employés.
+                Comparé à Apple et ses 147 000 employés, c'est plus du double de ce dernier.
+                Samsung Electronics est une des filiales les plus connues du Groupe Samsung
+                 car c'est elle qui produit les téléphones et les produits
+                d'électro-ménager de la même marque. Un des plus gros bénéfice annuel du Groupe Samsung,
+                la raison aussi de sa popularité.
+                Les téléphones Samsung autant populaire que leurs concurrents,
+                les téléphones de la marque Apple sont donc produit par cette filiale,
+                Samsung Electronics.
+            </p>
+        </div>
+    </div>
+    <hr>
+    <div class="row text-center align-items-center" id="capitalisation">
+        <div class="col-md-12 reveal">
+            <h3>
+                La capitalisation
+            </h3>
+        </div>
+
+    </div>
+
+    <div class="row text-justify align-items-center">
+        <div class="col-md-6">
+            <p class="reveal">
+            Apple comptabilise aujourd'hui une capitalisation boursière de 2100 milliards de dollars
+            ce qui représente environ 1 765 milliards d'euros.
+            </p>
+        </div>
+        <div class="col-md-6">
+            <p class="reveal">
+            Le Groupe Samsung quand a lui comptabilise une capitalisation boursière de
+            443 milliards de dollars soit environ 365 milliards d'euros.
+            </p>
+        </div>
+
+        <div class="col-12">
+            <p class="textCapitalisation reveal">
+                Par définition la capitalisation boursière est le produit du nombre des actions de l'entreprise
+                en circulation multiplié par le cours de l'action sur le marché.
+                <br>
+                Comme vous pouvez juste le voir avec l'image ci-contre.
+            </p>
+        </div>
+    </div>
+
+    <div class="row text-center align-items-center">
+        <div class="col-md-12">
+            <img src="../IMG/capital.png" alt="Image de capitalisation boursière" class="img-fluid img-thumbnail">
+        </div>
+    </div>
+
+    <hr>
+
+    <div class="row text-center align-items-center" id="trimestre">
+        <div class="col-md-12 reveal">
+            <h3>
+                Les trimestres
+            </h3>
+        </div>
+    </div>
+    <div class="row text-justify align-items-center">
+        <div class="col-md-6">
+            <p class="reveal">
+            Apple a eu des chiffres records pour la fin d'année 2020 avec un chiffre d'affaires de
+            64,7 milliards de dollars lors du quatrième trimestre. En comparaison avec 2019 qui était
+            déjà un record aussi (64 milliards de dollars) c'est encore une augmentation de bénéfices,
+            malgré la pandémie mondiale
+            </p>
+        </div>
+
+        <div class="col-md-6">
+            <p class="reveal">
+            Samsung Electronics a aussi eu une augmentation de ses chiffres de l'année 2019 à 2020, par exemple pour le
+            premier trimestre de 2019 elle enregistre un chiffre d'affaires de 48,4 milliards de dollars. Alors
+            que le même trimestre de 2020 elle enregistre un chiffre d'affaires de 50 milliards de dollars.
+            </p>
+        </div>
+    </div>
+
+    <hr>
+
+    <div class="row text-center align-items-center" id="bilan">
+        <div class="col-md-12">
+            <h3>
+                Bilan de l'année 2020 sur les actions de chacun
+            </h3>
+        </div>
+    </div>
+
+    <div class="row text-justify align-items-center">
+        <div class="col-md-6">
+            <p class="reveal">
+                On voit ici qu'Apple a effectivement
+                passé une période prolifique durant le confinement, comme le montre le
+                graphique ci-dessous au mois de mai, lors du début de l'évolution.
+                Mais on voit encore plus le changement lors de la traditionnelle conférence
+                annuelle en début d'année scolaire.
+            </p>
+        </div>
+
+        <div class="col-md-6">
+            <p class="reveal">
+                On voit ici que lors du confinement, la filiale Electronics de Samsung
+                a connu des périodes de hauts et de bas. Mais les actions ont globalement
+                augmentées lors du début d'année civique, similairement a Apple lors de
+                l'annonce de nouveaux produits.
+                Ici l'unité du gain en action est en Won Sud Coréen.
+            </p>
+        </div>
+        <div class="col-md-12">
+            <p class="reveal">
+                L'encadré bleu sur les deux images est la valeur d'une action
+                lors de la clôture du marché. Ici pour Apple la dernière transaction avait pour valeur
+                127,81 dollars. Du côté de Samsung Electronics la dernière transaction s'est vendue a
+                73 800 de Wons sud-coréen soit 67,13 dollars.
+            </p>
+        </div>
+    </div>
+
+    <div class="row align-items-center">
+        <div class="col-md-6">
+            <img src="../IMG/graphApple.png" class="img-fluid rounded float-left img-thumbnail">
+        </div>
+        <div class="col-md-6">
+            <img src="../IMG/graphSamsung.png" class="img-fluid  rounded float-right img-thumbnail">
+        </div>
+    </div>
+
+    <hr>
+
+</div>
+
+<div class="container">
+    <div class="row text-justify align-items-center">
+        <div class="col-md-12">
+            <p class="reveal">
+                On peut donc conclure que l'histoire économique des deux sociétés est
+                très différente. Mais sur le marché du smartphone, où elles sont chacune très
+                populaires elles se rapprochent économiquement. Elles se suivent au niveau des innovations
+                et au niveau des sorties de nouveaux produits, cette période particulière où les ventes
+                de chacun explosent.
+                <br>
+                Nous pouvons affirmer qu'Apple et Samsung resteront des concurrents pendant encore longtemps.
+            </p>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
+
+<?php
+include ('footer.php');
+?>
+<script>
+const ratio = .1;
+
+const options = {
+root: null,
+rootMargin: '0px', // marge pour que ce soir visible
+threshold: ratio // permet d'afficher lorsque 10% est visible
+}
+
+const handleIntersect = function (entries, observer) {
+entries.forEach(function (entry){
+    // Si ce qu'on voit avec intersectionRatio et supérieur au 10% demander
+    // grâce au ratio alors on passe la classe en visible
+    if (entry.intersectionRatio > ratio) {
+        entry.target.classList.add('reveal-visible')
+        observer.unobserve(entry.target)
+    }
+})
+}
+
+const observer = new IntersectionObserver(handleIntersect, options)
+document.querySelectorAll('.reveal').forEach(function(r){
+observer.observe(r)
+})
+</script>
+
+
+<!--
+<div class="col-md-6">
+Avec un nombre de titres égalant 17 001 800 000 titres
+</div>
+
+<div class="col-md-6">
+400 000 employés pour
+</div>
+
+-->
